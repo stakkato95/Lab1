f7b86bde12e06968d912ee1da6bffdc4cc7ac137
Kolyago Artyom <stakkato95@gmail.com>
2015-03-05 18:40:53 +0300

    added efficient & good-looking code

--- Lab1/Task2.aspx.cs
+++ Lab1/Task2.aspx.cs
@@ -8,6 +8,43 @@ using System.Web.UI.WebControls;
 public partial class Task2 : System.Web.UI.Page
 {
     private const String EMPTY_STRING = "";
+
+    private const double KgToDrahma = 564.4;
+    private const double KgToFunt = 2.205;
+    private const double KgToGra = 15340;
+    private const double KgToUnc = 35.27;
+    private const double KgToStoun = 0.1575;
+
+    private const double FuntToKg = 0.4536;
+    private const double FuntToDrahma = 256;
+    private const double FuntToGra = 7000;
+    private const double FuntToUnc = 16;
+    private const double FuntToStoun = 0.07143;
+
+    private const double UncToKg = 0.02835;
+    private const double UncToDrahma = 16;
+    private const double UncToGra = 437.5;
+    private const double UncToFunt = 0.0625;
+    private const double UncToStoun = 0.004464;
+
+    private const double DrahmaToKg = 0.001772;
+    private const double DrahmaToFunt = 0.003906;
+    private const double DrahmaToGra = 27.34;
+    private const double DrahmaToUnc = 0.0625;
+    private const double DrahmaToStoun = 0.000279;
+
+    private const double GraToKg = 0.0000648;
+    private const double GraToDrahma = 0.03657;
+    private const double GraToFunt = 0.0001429;
+    private const double GraToUnc = 0.002286;
+    private const double GraToStoun = 0.0000102;
+
+    private const double StounToKg = 6.35;
+    private const double StounToDrahma = 3584;
+    private const double StounToFunt = 14;
+    private const double StounToUnc = 224;
+    private const double StounToGra = 98000;
+
     private static double lastKgValue;
     private static double lastDrahmaValue;
     private static double lastFuntValue;
@@ -20,152 +57,61 @@ public partial class Task2 : System.Web.UI.Page
     }
     protected void ButtonConvert_Click(object sender, EventArgs e)
     {
-        string empty = "";
-
-        double KgToDrahma = 564.4;
-        double KgToFunt = 2.205;
-        double KgToGra = 15340;
-        double KgToUnc = 35.27;
-        double KgToStoun = 0.1575;
-
-        double FuntToKg = 0.4536;
-        double FuntToDrahma = 256;
-        double FuntToGra = 7000;
-        double FuntToUnc = 16;
-        double FuntToStoun = 0.07143;
-
-        double UncToKg = 0.02835;
-        double UncToDrahma = 16;
-        double UncToGra = 437.5;
-        double UncToFunt = 0.0625;
-        double UncToStoun = 0.004464;
-
-        double DrahmaToKg = 0.001772;
-        double DrahmaToFunt = 0.003906;
-        double DrahmaToGra = 27.34;
-        double DrahmaToUnc = 0.0625;
-        double DrahmaToStoun = 0.000279;
-
-        double GraToKg = 0.0000648;
-        double GraToDrahma = 0.03657;
-        double GraToFunt = 0.0001429;
-        double GraToUnc = 0.002286;
-        double GraToStoun = 0.0000102;
-
-        double StounToKg = 6.35;
-        double StounToDrahma = 3584;
-        double StounToFunt = 14;
-        double StounToUnc = 224;
-        double StounToGra = 98000;
-
-
         if (TextBoxKg.Text != lastKgValue.ToString() && TextBoxKg.Text != EMPTY_STRING)
         {
-            double source = double.Parse(TextBoxKg.Text);
-            TextBoxStoun.Text = (source * KgToStoun).ToString();
-            TextBoxFunt.Text = (source * KgToFunt).ToString();
-            TextBoxGran.Text = (source * KgToGra).ToString();
-            TextBoxDrahma.Text = (source * KgToDrahma).ToString();
-            TextBoxUnc.Text = (source * KgToUnc).ToString();
-
-            lastKgValue = source;
-            
-            double.TryParse(TextBoxDrahma.Text, out lastDrahmaValue);
-            double.TryParse(TextBoxFunt.Text, out lastFuntValue);
-            double.TryParse(TextBoxGran.Text, out lastGranValue);
-            double.TryParse(TextBoxUnc.Text, out lastUncValue);
-            double.TryParse(TextBoxStoun.Text, out lastStounValue);
+            lastKgValue = double.Parse(TextBoxKg.Text);
+            TextBoxStoun.Text = (lastStounValue = lastKgValue * KgToStoun).ToString();
+            TextBoxFunt.Text = (lastFuntValue = lastKgValue * KgToFunt).ToString();
+            TextBoxGran.Text = (lastGranValue = lastKgValue * KgToGra).ToString();
+            TextBoxDrahma.Text = (lastDrahmaValue = lastKgValue * KgToDrahma).ToString();
+            TextBoxUnc.Text = (lastUncValue = lastKgValue * KgToUnc).ToString();
         }
         else if (TextBoxDrahma.Text != lastDrahmaValue.ToString() && TextBoxDrahma.Text != EMPTY_STRING)
         {
-            double source = double.Parse(TextBoxDrahma.Text);
-            TextBoxKg.Text = (source * DrahmaToKg).ToString();
-            TextBoxFunt.Text = (source * DrahmaToFunt).ToString();
-            TextBoxGran.Text = (source * DrahmaToGra).ToString();
-            TextBoxStoun.Text = (source * DrahmaToStoun).ToString();
-            TextBoxUnc.Text = (source * DrahmaToUnc).ToString();
-
-            lastDrahmaValue = source;
-
-            double.TryParse(TextBoxKg.Text, out lastKgValue);
-            double.TryParse(TextBoxFunt.Text, out lastFuntValue);
-            double.TryParse(TextBoxGran.Text, out lastGranValue);
-            double.TryParse(TextBoxUnc.Text, out lastUncValue);
-            double.TryParse(TextBoxStoun.Text, out lastStounValue);
+            lastDrahmaValue = lastDrahmaValue = double.Parse(TextBoxDrahma.Text);
+            TextBoxKg.Text = (lastKgValue = lastDrahmaValue * DrahmaToKg).ToString();
+            TextBoxFunt.Text = (lastFuntValue = lastDrahmaValue * DrahmaToFunt).ToString();
+            TextBoxGran.Text = (lastGranValue = lastDrahmaValue * DrahmaToGra).ToString();
+            TextBoxStoun.Text = (lastStounValue = lastDrahmaValue * DrahmaToStoun).ToString();
+            TextBoxUnc.Text = (lastUncValue = lastDrahmaValue * DrahmaToUnc).ToString();
         }
         else if (TextBoxFunt.Text != lastFuntValue.ToString() && TextBoxFunt.Text != EMPTY_STRING)
         {
-            double source = double.Parse(TextBoxFunt.Text);
-            TextBoxKg.Text = (source * FuntToKg).ToString();
-            TextBoxDrahma.Text = (source * FuntToDrahma).ToString();
-            TextBoxGran.Text = (source * FuntToGra).ToString();
-            TextBoxStoun.Text = (source * FuntToStoun).ToString();
-            TextBoxUnc.Text = (source * FuntToUnc).ToString();
-
-            lastFuntValue = source;
-
-            double.TryParse(TextBoxDrahma.Text, out lastDrahmaValue);
-            double.TryParse(TextBoxKg.Text, out lastKgValue);
-            double.TryParse(TextBoxGran.Text, out lastGranValue);
-            double.TryParse(TextBoxUnc.Text, out lastUncValue);
-            double.TryParse(TextBoxStoun.Text, out lastStounValue);
+            lastFuntValue = double.Parse(TextBoxFunt.Text);
+            TextBoxKg.Text = (lastFuntValue = lastFuntValue * FuntToKg).ToString();
+            TextBoxDrahma.Text = (lastFuntValue = lastFuntValue * FuntToDrahma).ToString();
+            TextBoxGran.Text = (lastFuntValue = lastFuntValue * FuntToGra).ToString();
+            TextBoxStoun.Text = (lastFuntValue = lastFuntValue * FuntToStoun).ToString();
+            TextBoxUnc.Text = (lastFuntValue = lastFuntValue * FuntToUnc).ToString();
         }
         else if (TextBoxGran.Text != lastGranValue.ToString() && TextBoxGran.Text != EMPTY_STRING)
         {
-            double source = double.Parse(TextBoxGran.Text);
-            TextBoxKg.Text = (source * GraToKg).ToString();
-            TextBoxDrahma.Text = (source * GraToDrahma).ToString();
-            TextBoxFunt.Text = (source * GraToFunt).ToString();
-            TextBoxStoun.Text = (source * GraToStoun).ToString();
-            TextBoxUnc.Text = (source * GraToUnc).ToString();
-
-            lastGranValue = source;
-
-            double.TryParse(TextBoxDrahma.Text, out lastDrahmaValue);
-            double.TryParse(TextBoxFunt.Text, out lastFuntValue);
-            double.TryParse(TextBoxKg.Text, out lastKgValue);
-            double.TryParse(TextBoxUnc.Text, out lastUncValue);
-            double.TryParse(TextBoxStoun.Text, out lastStounValue);
+            lastGranValue = double.Parse(TextBoxGran.Text);
+            TextBoxKg.Text = (lastGranValue = lastGranValue * GraToKg).ToString();
+            TextBoxDrahma.Text = (lastGranValue = lastGranValue * GraToDrahma).ToString();
+            TextBoxFunt.Text = (lastGranValue = lastGranValue * GraToFunt).ToString();
+            TextBoxStoun.Text = (lastGranValue = lastGranValue * GraToStoun).ToString();
+            TextBoxUnc.Text = (lastGranValue = lastGranValue * GraToUnc).ToString();
         }
         else if (TextBoxUnc.Text != lastUncValue.ToString() && TextBoxUnc.Text != EMPTY_STRING)
         {
-            double source = double.Parse(TextBoxUnc.Text);
-            TextBoxKg.Text = (source * UncToKg).ToString();
-            TextBoxDrahma.Text = (source * UncToDrahma).ToString();
-            TextBoxGran.Text = (source * UncToGra).ToString();
-            TextBoxFunt.Text = (source * UncToFunt).ToString();
-            TextBoxStoun.Text = (source * UncToStoun).ToString();
-
-            lastUncValue = source;
-
-            double.TryParse(TextBoxDrahma.Text, out lastDrahmaValue);
-            double.TryParse(TextBoxFunt.Text, out lastFuntValue);
-            double.TryParse(TextBoxGran.Text, out lastGranValue);
-            double.TryParse(TextBoxKg.Text, out lastKgValue);
-            double.TryParse(TextBoxStoun.Text, out lastStounValue);
+            lastUncValue = double.Parse(TextBoxUnc.Text);
+            TextBoxKg.Text = (lastUncValue = lastUncValue * UncToKg).ToString();
+            TextBoxDrahma.Text = (lastUncValue = lastUncValue * UncToDrahma).ToString();
+            TextBoxGran.Text = (lastUncValue = lastUncValue * UncToGra).ToString();
+            TextBoxFunt.Text = (lastUncValue = lastUncValue * UncToFunt).ToString();
+            TextBoxStoun.Text = (lastUncValue = lastUncValue * UncToStoun).ToString();
         }
         else if (TextBoxStoun.Text != lastStounValue.ToString() && TextBoxStoun.Text != EMPTY_STRING)
         {
-            double source = double.Parse(TextBoxStoun.Text);
-            TextBoxKg.Text = (source * StounToKg).ToString();
-            TextBoxDrahma.Text = (source * StounToDrahma).ToString();
-            TextBoxGran.Text = (source * StounToGra).ToString();
-            TextBoxFunt.Text = (source * StounToFunt).ToString();
-            TextBoxUnc.Text = (source * StounToUnc).ToString();
-
-            lastStounValue = source;
-
-            double.TryParse(TextBoxDrahma.Text, out lastDrahmaValue);
-            double.TryParse(TextBoxFunt.Text, out lastFuntValue);
-            double.TryParse(TextBoxGran.Text, out lastGranValue);
-            double.TryParse(TextBoxUnc.Text, out lastUncValue);
-            double.TryParse(TextBoxKg.Text, out lastKgValue);
+            lastStounValue = double.Parse(TextBoxStoun.Text);
+            TextBoxKg.Text = (lastStounValue = lastStounValue * StounToKg).ToString();
+            TextBoxDrahma.Text = (lastStounValue = lastStounValue * StounToDrahma).ToString();
+            TextBoxGran.Text = (lastStounValue = lastStounValue * StounToGra).ToString();
+            TextBoxFunt.Text = (lastStounValue = lastStounValue * StounToFunt).ToString();
+            TextBoxUnc.Text = (lastStounValue = lastStounValue * StounToUnc).ToString();
         }
-
-
     }
-
-
 
     protected void ButtonClear_Click(object sender, EventArgs e)
     {
@@ -175,11 +121,8 @@ public partial class Task2 : System.Web.UI.Page
         TextBoxStoun.Text = "";
         TextBoxUnc.Text = "";
         TextBoxDrahma.Text = "";
+        lastDrahmaValue = lastFuntValue = lastGranValue = lastKgValue = lastStounValue = lastUncValue = 0;
     }
-
-
-
-
 
     protected void MassValidator_ServerValidate(object source, ServerValidateEventArgs args)
     {
